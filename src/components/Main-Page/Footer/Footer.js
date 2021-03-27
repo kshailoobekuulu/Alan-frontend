c6f3dd9ff01abf3924c33a5134f7f5f584a5ea81
BolotKyzy <baktygul160799@gmail.com>
2021-03-27 13:41:43 +0600

    Created menu and footer

--- src/components/Main-Page/Footer/Footer.js
+++ src/components/Main-Page/Footer/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './Footer.css';
+import logo from '../../../assets/images/brand.svg';
+import insta from "../../../assets/images/instagram.png";
+import whatsapp from "../../../assets/images/cib_whatsapp.png";
+import facebook from "../../../assets/images/facebook.png";
 
 const Footer = () => {
     return (
         <div className='footer'>
-            Footer
+            <div className='footer-container'>
+                <div className="footer-top">
+                    <div className="footer-contacts-item">
+                        <div className="footer-conracts-text">Phone</div>
+                        <a href="tel:0700482721" className="footer-contacts-value">0700-482-721</a>
+                    </div>
+                    <div className="footer-contacts-item">
+                        <div className="footer-conracts-text">Address</div>
+                        <address className="footer-contacts-value">Токтогул, Бекмамат Осмонов 48</address>
+                    </div>
+                    <div className='footer-contacts-item'>
+                            <a  href="#"> <img src={insta} alt='insta' className='inst'/></a>
+                            <a  href="#"> <img src={whatsapp} alt='whats' className=' icon '/></a>
+                            <a  href="#"><img src={facebook} alt='facebook' className='icon'/></a>
+
+
+                    </div>
+                </div>
+
+                <div className="footer-bottom">
+                <a className="logo">
+                    <img src={logo} alt="Alan"/>
+                </a>
+                <small className="copyright">&copy;&nbsp;2020@webovio. All Rights Reserved.</small>
+            </div>
+            </div>
         </div>
     )
 }
--- src/components/Main-Page/OurMenu/OurMenu.js
+++ src/components/Main-Page/OurMenu/OurMenu.js
@@ -5,7 +5,40 @@ const OurMenu = () => {
     return (
         <div className= 'our-menu'>
             <div className='our-menu-container'>
-                <h2>Наше Меню</h2>
+                <h2 className= 'menu-header'>Наше Меню</h2>
+                <ul className='menu-list'>
+                    <li className='menu-item item1'>
+                        <dev className = 'menu-item-bottom'>
+                            <h3 className='menu-item-bottom-text'>суши</h3>
+                        </dev>
+                    </li>
+                    <li className='menu-item item2'>
+                        <dev className = 'menu-item-bottom'>
+                            <h3 className='menu-item-bottom-text'>бургеры</h3>
+                        </dev>
+                    </li>
+                    <li className='menu-item item3'>
+                        <dev className = 'menu-item-bottom'>
+                            <h3 className='menu-item-bottom-text'>пиццы</h3>
+                        </dev>
+                    </li>
+                    <li className='menu-item item4'>
+                        <dev className = 'menu-item-bottom'>
+                            <h3 className='menu-item-bottom-text'>шашлыки</h3>
+                        </dev>
+                    </li>
+                    <li className='menu-item item5'>
+                        <dev className = 'menu-item-bottom'>
+                            <h3 className='menu-item-bottom-text'>сендвичи</h3>
+                        </dev>
+                    </li>
+                    <li className='menu-item item6'>
+                        <dev className = 'menu-item-bottom'>
+                            <h3 className='menu-item-bottom-text'>шаурма</h3>
+                        </dev>
+                    </li>
+
+                </ul>
             </div>
 
         </div>
