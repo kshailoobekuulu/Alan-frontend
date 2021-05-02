420043929335622d8da89acb02c3a864d2bb1e8f
Aidana Niiazova <aidana.niiazova@gmail.com>
2021-05-02 18:33:27 +0600

    fixed all products page

--- src/components/pages/Menu-Page/food-list-item/food-list-item.js
+++ src/components/pages/Menu-Page/food-list-item/food-list-item.js
@@ -1,29 +1,53 @@
 import React from "react";
 import addToCard from "../../../../assets/images/shopping-cart-48.png";
 import '../menu-page.css';
+import placeholder from '../../../../assets/images/placeholder.jpg'
 
 const FoodListItem = ({food, onAddedToCart}) => {
     const {name, price, photo} = food;
-    console.log(photo);
+    // console.log("rfefe", photo);
 
     return (
-        <ul className='menu-items'>
 
-            <li className='menu-foods-image'><img src={photo} alt='food'/></li>
-            <li className='menu-foods-name'>
+        <div className='menu-items'>
+            <div className='menu-foods-image'>
+                {photo !== undefined ?
+                              <img src={photo} alt='food'/> : <img src={placeholder} alt='food'/> }
+            </div>
+
+            <div className='menu-foods-name'>
                 <span>Fruits</span><br/>
                 <p>{name}</p>
-            </li>
-            <li className='menu-foods-price'>{price} сом</li>
-            <li className='menu-add-to-cart'>
-                <button
-                    onClick={onAddedToCart}
-                    className="add-to-cart-button">
-                    <div className="add-to-card-icon">
-                    </div>
+            </div>
+
+            <div className='menu-foods-price'>{price} сом</div>
+
+            <div>
+                <button onClick={onAddedToCart} className="add-to-cart-button">
+                    <div className="add-to-card-icon"></div>
                 </button>
-            </li>
-        </ul>
+            </div>
+        </div>
+        // <ul className='menu-items'>
+        //
+        //     <li className='menu-foods-image'>
+        //         {photo !== undefined ?
+        //             <img src={photo} alt='food'/> : <img src={placeholder} alt='food'/> }
+        //     </li>
+        //     <li className='menu-foods-name'>
+        //         <span>Fruits</span><br/>
+        //         <p>{name}</p>
+        //     </li>
+        //     <li className='menu-foods-price'>{price} сом</li>
+        //     <li className='menu-add-to-cart'>
+        //         <button
+        //             onClick={onAddedToCart}
+        //             className="add-to-cart-button">
+        //             <div className="add-to-card-icon">
+        //             </div>
+        //         </button>
+        //     </li>
+        // </ul>
 
     )
 
